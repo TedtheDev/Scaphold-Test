ec6b2836191269b4fb91a9f19846138a6a9ef63c
TedtheDev <tylerjedmunds@gmail.com>
2017-04-09 15:02:23 -0500

    added players resolver, added player type to schema, added players query to schema, added deletePerson to mutation, added deletePerson resolver, configured API stuff

--- GraphQLApp/graph/resolvers.js
+++ GraphQLApp/graph/resolvers.js
@@ -1,37 +1,64 @@
 const Person = require('../mongodb/models/person');
+const axios = require('axios');
+const api = require('../creds/apikey')
+
+const API_KEY = api.key;
+const API_ROOT = 'https://api.worldoftanks.com/wot/account/list/?';
+const APPLICATION_ID = `application_id=${API_KEY}&`;
+const TYPES = 'type=startswith&language=en';
+const SEARCH = '&search=';
+
+const FULL_URL = API_ROOT + APPLICATION_ID + TYPES;
 
 const resolverMap = {
   Query: {
     persons() {
       return new Promise((resolve, reject) => {
         Person.find({ })
-          .then((persons) => {
-            resolve(persons);
+          .then((thepersons) => {
+            resolve(thepersons);
         })
         .catch((err) => reject(err));
       });
+    },
+    players(_, args) {
+      return new Promise((resolve, reject) => {
+        let url;
+        if(args.nickname !== null || args.nickname !== undefined)
+          url = `${FULL_URL}${SEARCH}${args.nickname}`;
+        else
+          reject('Nickname is required');
+        axios.get(url)
+          .then((players) => {
+            resolve(players.data.data);
+          })
+          .catch((err) => reject(err));
+      })
     }
   },
   Mutation: {
-    createPerson(name, city, state, avgHoursPerWeek, username, favoriteTank) {
+    createPerson(_, args) {
       const aPerson = new Person({
-        name: name,
-        city: city,
-        state: state,
-        avgHoursPerWeek: avgHoursPerWeek,
-        username: username,
-        favoriteTank: favoriteTank
+        name: args.name,
+        city: args.city,
+        state: args.state,
+        avgHoursPerWeek: args.avgHoursPerWeek,
+        username: args.username,
+        favoriteTank: args.favoriteTank
       });
-      return new Promise((resolve, reject) => {
-        aPerson.save()
-          .then((thePerson) => {
-            resolve(thePerson);
+      return aPerson.save()
+        .then(() => Person.findById({ _id: aPerson._id }))
+        .then((person) => {
+          return person
         })
-        .catch((err) => {
-          console.log(err.errors);
-          reject(err)
-        });
-      });
+        .catch((err) => err);
+    },
+    deletePerson(_, args) {
+      return Person.findOneAndRemove({ name: args.name, username: args.username })
+        .then((person) => {
+          return person
+        })
+        .catch((err) => err);
     }
   }
 };
