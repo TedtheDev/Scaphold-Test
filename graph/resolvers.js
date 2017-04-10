ae65088cbcab0b12f2c26b92ca65ad7362c9f3f7
TedtheDev <tylerjedmunds@gmail.com>
2017-04-09 23:38:09 -0500

    added more stuff

--- graph/resolvers.js
+++ graph/resolvers.js
@@ -2,24 +2,41 @@
 
 const Person = require('../mongodb/models/person');
 const axios = require('axios');
+const lo = require('lodash');
+
 if(process.env.WARGAMING_API_KEY)
   var api = process.env.WARGAMING_API_KEY
 else
   var api = require('../creds/apikey')
 
 const API_KEY = (!process.env.WARGAMING_API_KEY) ? api.key : api;
-const API_ROOT = 'https://api.worldoftanks.com/wot/account/list/?';
-const APPLICATION_ID = `application_id=${API_KEY}&`;
-const TYPES = 'type=startswith&language=en';
-const SEARCH = '&search=';
+const API_ROOT = 'https://api.worldoftanks.com/wot/';
+const APPLICATION_ID = `&application_id=${API_KEY}`;
 
-const FULL_URL = API_ROOT + APPLICATION_ID + TYPES;
+//TYPES OF SEARCHES
+//----------------
+//get player account_id and nickname using exact search by username
+const API_PLAYER_SEARCH = '/account/list/?type=exact&language=en&search=';
+
+// get vehicles of player by account id
+const API_TANKS_BY_PLAYER_SEARCH = '/account/tanks/?fields=tank_id&language=en&account_id=';
+
+//get all vehicle names
+const API_GET_TANKS_NAME = '/encyclopedia/vehicles/?fields=name,tank_id&language=en';
+
+const API_GET_FAV_TANK_STATS = '/tanks/stats/?account_id=1005863017&fields=all&language=en&tank_id=';
 
 const resolverMap = {
   Query: {
-    persons() {
+    persons(_, args) {
+      const findParams = {};
+      const {name, username } = args;
+      if(name)
+        findParams["name"] = name;
+      if(username)
+        findParams["username"] = username;
       return new Promise((resolve, reject) => {
-        Person.find({ })
+        Person.find(findParams)
           .then((thepersons) => {
             resolve(thepersons);
         })
@@ -30,7 +47,7 @@ const resolverMap = {
       return new Promise((resolve, reject) => {
         let url;
         if(args.nickname !== null || args.nickname !== undefined)
-          url = `${FULL_URL}${SEARCH}${args.nickname}`;
+          url = `${API_ROOT}${API_PLAYER_SEARCH}${args.nickname}${APPLICATION_ID}`;
         else
           reject('Nickname is required');
         axios.get(url)
@@ -38,6 +55,26 @@ const resolverMap = {
             resolve(players.data.data);
           })
           .catch((err) => reject(err));
+      })
+    },
+    getPlayerFavTankStats(_, args) {
+      return new Promise((resolve,reject) => {
+        axios.get(`${API_ROOT}${API_PLAYER_SEARCH}${args.username}${APPLICATION_ID}`) //get player account_id
+        .then((accountId) => {
+          axios.get(`${API_ROOT}${API_GET_TANKS_NAME}${APPLICATION_ID}`)
+            .then((tankIds) => {
+              let theTanksList = lo.mapValues(tankIds.data.data, (tank) => {
+                return tank.name
+              })
+              theTanksList = lo.invert(theTanksList);
+
+              axios.get(`${API_ROOT}${API_GET_FAV_TANK_STATS}${theTanksList[args.favoriteTank]}${APPLICATION_ID}`)
+                .then((favTankData) => {
+                  console.log(favTankData.data.data[Object.keys(favTankData.data.data)[0]][0].all);
+                  resolve(favTankData.data.data[Object.keys(favTankData.data.data)[0]][0].all);
+                })
+            })
+        });
       })
     }
   },
@@ -62,6 +99,29 @@ const resolverMap = {
       return Person.findOneAndRemove({ name: args.name, username: args.username })
         .then((person) => {
           return person
+        })
+        .catch((err) => err);
+    },
+    updatePerson(_, args) {
+      const { targetName, targetUsername, name, city, state, avgHoursPerWeek, username, favoriteTank } = args;
+      let updateParams = {};
+      if(name)
+        updateParams["name"] = name;
+      if(city)
+        updateParams["city"] = city;
+      if(state)
+        updateParams["state"] = state;
+      if(avgHoursPerWeek)
+        updateParams["avgHoursPerWeek"] = avgHoursPerWeek;
+      if(username)
+        updateParams["username"] = username;
+      if(favoriteTank)
+        updateParams["favoriteTank"] = favoriteTank;
+
+      return Person.findOneAndUpdate({ name: targetName, username: targetUsername }, updateParams)
+        .then((person) => Person.findOne({ _id: person._id}))
+        .then((person) => {
+          return person;
         })
         .catch((err) => err);
     }
